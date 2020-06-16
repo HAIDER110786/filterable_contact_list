def05294db2d8ba04de5a8e07bed7c849bc9a391
Haider <41267875+HAIDER110786@users.noreply.github.com>
2020-06-16 21:23:52 +0500

    Update Contacts.js

--- JS/Contacts.js
+++ JS/Contacts.js
@@ -1,17 +1,25 @@
 document.querySelector('#SearchContact').addEventListener('keyup',()=>{
         const searchedContact = (document.querySelector('#SearchContact').value)
+        
         const allAvailableContacts = document.querySelectorAll('#contacts');
+        
         allAvailableContacts.forEach(contact=>{
+                
             let j = '';
             for(let i = 0 ; i < searchedContact.length ; i++){
                 j+=contact.textContent[i];
             }
             
             console.log(contact.childElementCount);
+                
             if(j!==searchedContact){
+                    
                 contact.style.display = 'none';
             }else{
+                    
                 contact.style.display = 'block';
             }
         })
     })
+
+
